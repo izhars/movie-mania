8dcc6b2b55d70baf11b3abee8988226a1605788e
Sharique Izhar <shariqueizhar@Shariques-MacBook-Air.local>
2024-08-07 18:28:22 +0530

    third commit

--- new_screens/UserDetailPage.js
+++ new_screens/UserDetailPage.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
 
-const UserDetailPage = ({ route }) => {
+const UserDetailPage = ({ route, navigation }) => {
   const { user } = route.params;
 
   return (
     <View style={styles.container}>
+      <TouchableOpacity style={styles.backButton} onPress={() => navigation.goBack()}>
+        <Text style={styles.backButtonText}>Back</Text>
+      </TouchableOpacity>
       <Image source={{ uri: user.picture.large }} style={styles.image} />
       <Text style={styles.text}>{`${user.name.first} ${user.name.last}`}</Text>
       <Text style={styles.text}>{`${user.email}`}</Text>
       <Text style={styles.text}>{`${user.phone}`}</Text>
       <Text style={styles.text}>{`${user.location.city}, ${user.location.country}`}</Text>
       <Text style={styles.text}>{`${user.location.coordinates.latitude}`}</Text>
-            <Text style={styles.text}>{`${user.location.coordinates.latitude}`}</Text>
     </View>
   );
 };
@@ -22,6 +24,16 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     padding: 20,
+  },
+  backButton: {
+    position: 'absolute',
+    top: 10,
+    left: 10,
+    padding: 10,
+  },
+  backButtonText: {
+    fontSize: 18,
+    color: 'blue',
   },
   image: {
     width: 100,
